26d6ad0b49019dedacbc8558ebdd2251fe0c7c85
LuizaNs <luizansjs@gmail.com>
2023-05-05 09:23:20 -0300

    declarando var/let/const hoisting e interpolação

---
+++ js/script.js
@@ -0,0 +1,60 @@
+console.log("Olá mundo!")
+
+//  //Declarar uma variável com a palavra reserva var.
+//  var nr1 = 10;
+//  var nr2 = 5;
+
+//  //Vamos imprimir as variáveis com o objetivo console. 
+//  console.log(nr1);
+//  console.log(nr2);
+
+//  //Realizando as operações básicas com estes valores
+//  // +, -, * e /
+
+//  // Adição
+//  //REALIZE A CONCATENAÇÃO COM O TEXTO RESULTADO:
+//  //ex: console.log(RESULTADO nr1 + nr2) 
+//  console.log("RESULTADO:", + (nr1 + nr2));
+//  console.log("RESULTADO: " + (parseInt(nr1) + parseInt(nr2)));
+//  console.log(`RESULTADO : ${parseInt(nr1) + parseInt(nr2)}`);
+
+//  //SUBTRAÇÃO
+//  console.log(`RESULTADO SUBTRAÇÃO: ${parseInt(nr1) - parseInt(nr2)}`);
+
+//  //MULTIPLICAÇÃO
+//  console.log(`RESULTADO MULTIPLICAÇÃO: ${parseInt(nr1) * parseInt(nr2)}`);
+
+//  //DIVISÃO
+//  console.log(`RESULTADO SUBTRAÇÃO: ${parseInt(nr1) / parseInt(nr2)}`);
+
+//  //OBTENHA O RESTO DA DIVISÃO
+//  console.log(`RESULTADO DO RESTO DA DIVISÃO: ${parseInt(nr1) % parseInt(nr2)}`)
+
+ //Descobrindo se um número é par ou ímpar 
+ //Utilizando operador ternário ( ? )
+
+//  var nr1 = "10"
+//  var nr2 = "5"
+//  var resultado; 
+
+//  resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "ÍMPAR";
+//  console.log(`O número é: ${resultado}`);
+
+//Regras do const 
+//Primeira regra = Incializar na declaração 
+const nome3 = "Violante";
+
+//Segunda regra = Não pode ser alterado durante o fluxo do programa 
+// nome3 = "Jonathan";
+
+//Diferença entre var, let, const
+// var nome1 = "Beatriz";
+// let nom2 = "Matheus";
+
+// let nome = "Beatriz";
+
+// if (nome == ""){
+//     let nome = "Robson"
+// }
+// //Qual é o nome que será impresso no log? 
+// console.log(nome);
